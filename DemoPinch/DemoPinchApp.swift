8f74679ed46c5dc373e5aba34ab706a8032d7d31
cesar <cursos.mapatupisaro@gamail.com>
2024-03-11 18:35:26 -0600

    Initial Commit

---
+++ DemoPinch/DemoPinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoPinchApp.swift
+//  DemoPinch
+//
+//  Created by Aguid Ramirez Sanchez on 11/03/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoPinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
